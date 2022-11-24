24eb48eacc1fc0c6d07569ee61effefe909d0171
Thomas Lautaro López <laautarolopez@gmail.com>
2022-11-24 20:32:22 -0300

    api creada(sin probar)

---
+++ src/Api.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const host = "https://preguntados-api.vercel.app";
+
+const getDifficulty = () => {
+    return axios.get(host + "/api/difficulty")
+        .then((response) => { return response.data })
+        .catch((error) => { return error });
+};
+
+const getQuestions = (difficulty) => {
+    const params = {
+        difficulty: difficulty,
+    };
+    return axios.get(host + "/api/questions", params)
+        .then((response) => { return response.data })
+        .catch((error) => { return error });
+};
+
+const postAnswer = (questionId, option) => {
+    return axios
+        .post(host + "/api/answer", {
+            questionId: questionId,
+            option: option,
+        })
+        .then((response) => { return response.data })
+        .catch((error) => { return error });
+};
+
+const Api = {
+    getDifficulty,
+    getQuestions,
+    postAnswer
+};
+
+export default Api;
