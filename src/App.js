fcffd88e4d2c93e7526ed5c0e8d46dbf2ac75d77
Thomas Lautaro López <laautarolopez@gmail.com>
2022-11-24 20:55:29 -0300

    spinner mientras se espera 'Loading ...'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Titulo from './Titulo/Titulo';
+import { Spinner } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
@@ -7,7 +9,9 @@ const App = () => {
         <Titulo />
         <hr className='divisor'/>
         <div className='container'>
-            
+            <div className='spinnerContainer'>
+                <Spinner />
+            </div>
         </div>
     </div>
   );
