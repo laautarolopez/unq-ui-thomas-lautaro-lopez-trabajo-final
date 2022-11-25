6159294c1539853b225f4609269575700182fec0
Thomas Lautaro López <laautarolopez@gmail.com>
2022-11-25 19:55:33 -0300

    dificultades responsive

--- src/Categorias/Categorias.js
+++ src/Categorias/Categorias.js
@@ -5,10 +5,12 @@ const Categorias = ({ dificultades }) => {
     return(
         <div className='categorias'>
             <div className='categorias-titulo'>Seleccione una dificultad</div>
-            {dificultades.map((dificultad, index) => {
-                return <Button className="categorias-dificultad" key={index}>{dificultad}</Button>
-            })
-            }
+            <div className='categorias-dificultades'>
+                {dificultades.map((dificultad, index) => {
+                    return <Button className="boton-general" key={index}>{dificultad}</Button>
+                })
+                }
+            </div>
             <Button className='boton-siguiente'>Siguiente</Button>
         </div>
     );
