2477613796ae750fbad0eccce30080d9d0b5b5ac
Thomas Lautaro López <laautarolopez@gmail.com>
2022-11-24 23:09:29 -0300

    mientras carga se muestra el spinner, luego las categorias

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import './App.css';
-import Titulo from './Titulo/Titulo';
 import { Spinner } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState, useEffect } from 'react';
+import Titulo from './Titulo/Titulo';
+import Api from './Api';
+import Categorias from './Categorias/Categorias';
 
 const App = () => {
-  return (
-    <div className="app">
-        <Titulo />
-        <hr className='divisor'/>
-        <div className='container'>
-            <div className='spinnerContainer'>
-                <Spinner />
+    const [dificultades, setDificultades] = useState(undefined);
+
+    useEffect(() => {
+        Api.getDifficulty().then(response => setDificultades(response));
+    }, []);
+
+    return (
+        <div className="app">
+            <Titulo />
+            <hr className='divisor'/>
+            <div className='container'>
+                {dificultades != undefined
+                ? <Categorias dificultades={dificultades} />  
+                : <div className='spinnerContainer'>
+                    <Spinner />
+                  </div>
+                }
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
