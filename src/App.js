b6ebaf17c678d25a114c1ee160544590fbb452fd
Thomas Lautaro López <laautarolopez@gmail.com>
2022-11-25 23:51:59 -0300

    Rutas en App y cambios de algunos estilos

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
 import './App.css';
-import { Spinner } from 'reactstrap';
+import { Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState, useEffect } from 'react';
 import Titulo from './Titulo/Titulo';
-import Api from './Api';
-import Categorias from './Categorias/Categorias';
+import Home from './Home/Home';
+import NotFound from './NotFound/NotFound';
 
 const App = () => {
-    const [dificultades, setDificultades] = useState(undefined);
-
-    useEffect(() => {
-        Api.getDifficulty().then(response => setDificultades(response));
-    }, []);
-
     return (
         <div className="app">
             <Titulo />
             <hr className='divisor'/>
-            <div className='container'>
-                {dificultades != undefined
-                ? <Categorias dificultades={dificultades} />  
-                : <div className='spinnerContainer'>
-                    <Spinner />
-                  </div>
-                }
-            </div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                {/* <Route path="/elemento/:parametro" element={<elemento />} /> */}
+                <Route path="*" element={<NotFound />} />
+            </Routes>
         </div>
     );
 }
