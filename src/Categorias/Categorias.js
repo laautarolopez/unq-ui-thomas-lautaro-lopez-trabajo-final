2e0cda2aaf774c9aad748b4d253e9c263c3237a8
Thomas Lautaro López <laautarolopez@gmail.com>
2022-11-25 23:53:23 -0300

    categorias terminado funcionando(con error y redireccion a la pagina destino)

--- src/Categorias/Categorias.js
+++ src/Categorias/Categorias.js
@@ -1,17 +1,38 @@
-import { Button } from 'reactstrap';
 import './Categorias.css';
+import { Button } from 'reactstrap';
+import { useState } from 'react';
+import Error from '../Error/Error';
+import { Navigate } from "react-router-dom";
 
 const Categorias = ({ dificultades }) => {
+    const [dificultadActiva, setDificultadActiva] = useState(undefined);
+    const [error, setError] = useState(false);
+    const [habilitado, setHabilitado] = useState(false);
+
+    const verificarSeleccion = () => {
+        dificultadActiva != undefined
+        ? setHabilitado(true)
+        : setError(true)
+    }
+
     return(
         <div className='categorias'>
             <div className='categorias-titulo'>Seleccione una dificultad</div>
             <div className='categorias-dificultades'>
                 {dificultades.map((dificultad, index) => {
-                    return <Button className="boton-general" key={index}>{dificultad}</Button>
+                    return <Button key={index}
+                                className={dificultad == dificultadActiva ? "boton-general dificultad-activa" : "boton-general"} 
+                                onClick={() => setDificultadActiva(dificultad)}>
+                                    {dificultad}
+                                </Button>
                 })
                 }
+                {error && <Error mensaje={"debe seleccionar una dificultad."} />}
             </div>
-            <Button className='boton-siguiente'>Siguiente</Button>
+            <Button onClick={verificarSeleccion} className='boton-siguiente'>
+                Siguiente
+            </Button>
+            {habilitado && <Navigate to={`/questions?difficulty=${dificultadActiva}`} />}
         </div>
     );
 }
